5ab5ec79d98250b07c6ee6dcfd1e215d6f51896a
mahmu <mahmu@192.168.1.10>
2021-03-06 03:07:48 -0500

    New Class by Mahmud Pervez;

---
+++ Selenium_Automation_Batch12/src/test/java/com/nyc/inheritance/NewArraysClass.java
@@ -0,0 +1,13 @@
+package com.nyc.inheritance;
+
+public class NewArraysClass {
+	public static void main(String[] args) {
+		int x;
+		x=50;
+		x=x+50;
+		for(int i=0;i<10;i++) {
+			System.out.println(i);
+		}
+	}
+
+}
--- Selenium_Automation_Batch12/src/test/java/com/nyc/inheritance/NewMahmudGit.java
+++ Selenium_Automation_Batch12/src/test/java/com/nyc/inheritance/NewMahmudGit.java
@@ -1,5 +1,11 @@
 package com.nyc.inheritance;
 
 public class NewMahmudGit {
-
+	
+public static void main(String[] args) {
+	int sum=100+50;
+	int sum1=sum+150;
+	System.out.println(sum);
+	System.out.println(sum1);
+}
 }
